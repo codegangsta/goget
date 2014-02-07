8d3c7027ebe4dfcfe932d71c2e670ae84bb702d4
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-02-07 13:28:28 -0800

    Added newrelic support

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"github.com/codegangsta/martini"
+	"github.com/yvasiyarov/gorelic"
+	"os"
 )
 
 func main() {
+	// setup newrelic
+	if martini.Env == martini.Prod {
+		agent := gorelic.NewAgent()
+		agent.Verbose = true
+		agent.NewrelicLicense = os.Getenv("NEW_RELIC_LICENSE")
+		agent.Run()
+	}
+
 	m := martini.Classic()
 
 	m.Get("/", func() string {
